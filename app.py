bbe1c6946a34557955a28a1050d65d641e5f1e4d
Benjamin Rosenthal <brosen8690@gmail.com>
2024-04-25 07:28:30 -0400

    Getting started with jwt

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+SECRET_KEY = os.environ.get('SECRET_KEY') or 'this is a secret'
+print(SECRET_KEY)
+app.config['SECRET_KEY'] = SECRET_KEY
+
 from auth import views
