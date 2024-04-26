f2e4b844dc6ecb6d466838578883af2456622e4e
benjamin-rosenthal <brosen8690@gmail.com>
2024-04-26 17:50:29 -0400

    added user model and sql packages

---
+++ models/user.py
@@ -0,0 +1,13 @@
+from typing import Optional
+
+from sqlalchemy import Column, PrimaryKeyConstraint, String
+from sqlalchemy.orm import mapped_column
+from sqlmodel import Field, SQLModel
+
+class Email(SQLModel, table=True):
+    __table_args__ = (
+        PrimaryKeyConstraint('email', name='email_pkey'),
+    )
+
+    email: str = Field(sa_column=mapped_column('email', String(256)))
+    password: Optional[str] = Field(default=None, sa_column=mapped_column('password', String(256)))
