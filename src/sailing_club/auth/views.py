1e0e69e987473514b0da6f845768de35c6d9844e
Benjamin Rosenthal <brosen8690@gmail.com>
2024-05-25 08:35:32 -0400

    setting up db test but getting a circular import

--- src/sailing_club/auth/views.py
+++ src/sailing_club/auth/views.py
@@ -1,11 +1,13 @@
 from flask import jsonify, request, render_template, Blueprint
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
-from sailing_club.app import app
+from sqlmodel import Session, select
+from sailing_club.app import db
+from sailing_club.auth.models.user import User
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 # from sqlalchemy import text
-# from sailing_club.app import engine
+from sailing_club.app import engine
 
 # @app.route("/")
 # def hello_world():
@@ -19,25 +21,25 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 @bp.route("/login", methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.json.get("username", None)
-        password = request.json.get("password", None)
-        if username != "test" or password != "test":
-            return jsonify({"msg": "Bad username or password"}), 401
+        with Session(engine) as session:
+            loggingInUser = session.exec(select(User).where(User.username == request.json.get("username", None)))
+            if loggingInUser == None:
+                return jsonify({"msg": "Bad username or password"}), 401
 
-        access_token = create_access_token(identity=username)
-        return jsonify(access_token=access_token)
+            access_token = create_access_token(identity=loggingInUser.username)
+            return jsonify(access_token=access_token)
     
     return render_template('auth/login.html')
 
 
 # Protect a route with jwt_required, which will kick out requests
 # without a valid JWT present.
-# @app.route("/protected", methods=["GET"])
-# @jwt_required()
-# def protected():
-#     # Access the identity of the current user with get_jwt_identity
-#     current_user = get_jwt_identity()
-#     return jsonify(logged_in_as=current_user), 200
+@bp.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
 
 # # Test endpoint to get all users
 # @app.route("/users", methods=["GET"])
